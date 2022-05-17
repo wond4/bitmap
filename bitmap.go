e2f00c66994fd73efd9c155d4f67708716da48cf
wond4 <38349899+wond4@users.noreply.github.com>
2022-05-17 09:00:03 +0800

    fix(bitmap): 修复8的倍数造成错误

--- bitmap.go
+++ bitmap.go
@@ -9,7 +9,7 @@ type Bitmap struct {
 	bytes []byte
 }
 
-// NewBitmap: get a new bitmap
+// NewBitmap get a new bitmap
 func NewBitmap(length int) *Bitmap {
 	l := length >> 3
 	if length&7 != 0 {
@@ -22,9 +22,9 @@ func (b *Bitmap) index(num int) (int, int) {
 	return num >> 3, num & 7
 }
 
-// add: add a num to the bitmap
+// Add a num to the bitmap
 func (b *Bitmap) Add(num int) bool {
-	if num > b.len {
+	if num >= b.len {
 		return false
 	}
 	index, bit := b.index(num)
@@ -32,18 +32,18 @@ func (b *Bitmap) Add(num int) bool {
 	return true
 }
 
-// Has: check is a num in the bitmap
+// Has check is a num in the bitmap
 func (b *Bitmap) Has(num int) bool {
-	if num > b.len {
+	if num >= b.len {
 		return false
 	}
 	index, bit := b.index(num)
 	return b.bytes[index]&(1<<bit) != 0
 }
 
-// Del: delete a num from the bitmap
+// Del deletes a num from the bitmap
 func (b *Bitmap) Del(num int) bool {
-	if num > b.len {
+	if num >= b.len {
 		return false
 	}
 	index, bit := b.index(num)
@@ -51,26 +51,26 @@ func (b *Bitmap) Del(num int) bool {
 	return true
 }
 
-// Len: get the length of the bitmap
+// Len get the length of the bitmap
 func (b *Bitmap) Len() int {
 	return b.len
 }
 
-// LenRaw: get the memory size of the bitmap array in bytes
+// LenRaw get the memory size of the bitmap array in bytes
 func (b *Bitmap) LenRaw() int {
 	return len(b.bytes)
 }
 
-// String: convert the bitmap to a binary string
+// String convert the bitmap to a binary string
 func (b *Bitmap) String() string {
 	bs := make([]byte, len(b.bytes))
 	for i, v := range b.bytes {
 		bs[i] = reverse(v)
 	}
 	s := fmt.Sprintf("%08b", bs)
-	l :=b.len&7
-	if l!=0{
-		l=8-l
+	l := b.len & 7
+	if l != 0 {
+		l = 8 - l
 	}
 	return s[1 : len(s)-1-l]
 }
